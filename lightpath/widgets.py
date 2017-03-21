ef6fab8a26bb9bb705d6a75268812e019ec90618
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-03-21 14:10:37 -0700

    Starting widgets file

---
+++ lightpath/widgets.py
@@ -0,0 +1,18 @@
+"""
+Definitions for Lightpath Widgets
+"""
+############
+# Standard #
+############
+import logging
+
+###############
+# Third Party #
+###############
+import pedl
+
+##########
+# Module #
+##########
+
+logger = logging.getLogger(__name__)
