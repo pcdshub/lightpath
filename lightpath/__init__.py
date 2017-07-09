a90bc8dfeb483c7210e1009bbcf7cf276c352bfc
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-07-08 22:31:21 -0700

    API: Remove DEBUG statement, no longer neccesary with conftest logger setup

--- lightpath/__init__.py
+++ lightpath/__init__.py
@@ -7,15 +7,6 @@ from .utils      import MPS
 from .controller import LightController
 from .interface  import LightInterface, BranchingInterface
 
-#Setup logger
-logger = logging.getLogger(__name__)
-logger.addHandler(logging.StreamHandler())
-logger.setLevel(logging.CRITICAL)
-
-#For development
-def DEBUG():
-    logger.setLevel(logging.DEBUG)
-
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
