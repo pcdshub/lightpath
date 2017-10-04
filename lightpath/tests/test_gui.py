d66bfaf3e022546fabbfd8e31444682f4f7d1cf1
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-10-04 16:15:16 -0700

    TST: Include happi containers in tests and demos
    Slight change to widget tests as the remove method is now a pyqtSlot at the
    application level

--- lightpath/tests/test_gui.py
+++ lightpath/tests/test_gui.py
@@ -19,24 +19,29 @@ from lightpath.ui import LightApp
 def app():
     return QApplication([])
 
-def test_app_buttons(app, lcls):
-    lightapp = LightApp(*lcls)
+def test_app_buttons(app, lcls, containers):
+    lightapp = LightApp(*lcls, containers=containers)
     #Check we initialized correctly
     assert lightapp.upstream()
     assert not lightapp.mps_only()
     #Try to change display
-    assert len(lightapp.select_devices('MEC')) == 10
+    assert len(lightapp.select_devices('MEC')) == 11
+
+def test_remove_button(app, lcls, containers):
+    lightapp = LightApp(*lcls, containers=containers)
+    lightapp.remove(True, device=lightapp.rows[0].device)
+    assert lightapp.rows[0].device.removed
 
 @using_fake_epics_pv
 def test_app_from_json(app):
     #Basic configuration
-    app = LightApp.from_json(os.path.join(
+    lit = LightApp.from_json(os.path.join(
                              os.path.dirname(os.path.abspath(__file__)),
                              'path.json'))
-    assert len(app.light.devices) == 16
+    assert len(lit.light.devices) == 16
     #Limit device search
-    app = LightApp.from_json(os.path.join(
+    lit = LightApp.from_json(os.path.join(
                              os.path.dirname(os.path.abspath(__file__)),
                              'path.json'),
                              end=900.0)
-    assert len(app.light.devices) == 9
+    assert len(lit.light.devices) == 9
