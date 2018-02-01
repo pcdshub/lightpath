0648b61d35063974ed00e9c201066f00deeb1eae
Teddy Rendahl <trendahl@slac.stanford.edu>
2018-02-01 05:43:54 -0800

    TST: Remove mps_only check from tests

--- lightpath/tests/test_gui.py
+++ lightpath/tests/test_gui.py
@@ -14,39 +14,23 @@ from pcdsdevices.sim.pv import using_fake_epics_pv
 ##########
 from lightpath.ui import LightApp
 
-def test_app_buttons(lcls, containers):
-    lightapp = LightApp(*lcls, containers=containers)
+def test_app_buttons(lcls):
+    lightapp = LightApp(*lcls)
     #Check we initialized correctly
     assert lightapp.upstream()
-    assert not lightapp.mps_only()
     #Create widgets
-    assert len(lightapp.select_devices('MEC')) == 11
+    assert len(lightapp.select_devices('MEC')) == 10
     #Setup new display
     mec_idx = lightapp.destination_combo.findText('MEC')
     lightapp.destination_combo.setCurrentIndex(mec_idx)
     lightapp.change_path_display()
-    assert len(lightapp.rows) == 11
+    assert len(lightapp.rows) == 10
 
-def test_beampath_controls(lcls, containers):
-    lightapp = LightApp(*lcls, containers=containers)
+def test_beampath_controls(lcls):
+    lightapp = LightApp(*lcls)
     lightapp.remove(True, device=lightapp.rows[0].device)
     assert lightapp.rows[0].device.removed
     lightapp.insert(True, device=lightapp.rows[0].device)
     assert lightapp.rows[0].device.inserted
     lightapp.transmission_adjusted(50)
     assert lightapp.path.minimum_transmission == 0.5
-
-@using_fake_epics_pv
-@pytest.mark.xfail
-def test_app_from_json():
-    #Basic configuration
-    lit = LightApp.from_json(os.path.join(
-                             os.path.dirname(os.path.abspath(__file__)),
-                             'path.json'))
-    assert len(lit.light.devices) == 16
-    #Limit device search
-    lit = LightApp.from_json(os.path.join(
-                             os.path.dirname(os.path.abspath(__file__)),
-                             'path.json'),
-                             end=900.0)
-    assert len(lit.light.devices) == 9
