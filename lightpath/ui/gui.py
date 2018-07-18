04af12557427c2a1f35b5e75d8ade56e67cc0b8d
teddyrendahl <trendahl@slac.stanford.edu>
2018-07-18 14:37:41 -0700

    MAINT: Highlight both sides of beam path in device drawing

--- lightpath/ui/gui.py
+++ lightpath/ui/gui.py
@@ -200,12 +200,20 @@ class LightApp(Display):
             for row in self.rows:
                 # If our device is before or at the impediment, it is lit
                 if not block or (row.device.md.z <= block.md.z):
-                    row.indicator._default_color = Qt.cyan
+                    # This device is being hit by the beam
+                    row.beam_indicator._default_color = Qt.cyan
+                    # Check whether this device is passing beam
+                    if block != row.device:
+                        row.out_indicator._default_color = Qt.cyan
+                    else:
+                        row.out_indicator._default_color = Qt.gray
                 # Otherwise, it is off
                 else:
-                    row.indicator._default_color = Qt.gray
+                    row.beam_indicator._default_color = Qt.gray
+                    row.out_indicator._default_color = Qt.gray
                 # Update widget display
-                row.indicator.update()
+                row.beam_indicator.update()
+                row.out_indicator.update()
 
     def clear_subs(self):
         """
