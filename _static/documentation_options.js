40bc28413eba25add06b43aa98c0aa6eddecb7d2
Travis CI User <travis@example.org>
2018-09-11 18:25:34 +0000

    Update docs after building Travis build 161 of pcdshub/lightpath
    The docs were built from the branch 'master' against the commit
    8ad0cb946f9bb19ef975c7f69feedd6bb13e29e3.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/pcdshub/lightpath/jobs/427332397.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test-environment/bin/doctr deploy . --built-docs docs/build/html --deploy-branch-name gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.0+87.gc8f173c',
+    VERSION: '0.4.0+98.g8ad0cb9',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
