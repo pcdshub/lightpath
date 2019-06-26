d887a8099ea20b49f73e8364b909e16c61dd3da8
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-06-26 22:26:57 +0000

    Update docs after building Travis build 197 of pcdshub/lightpath
    The docs were built from the branch 'master' against the commit
    f99f9c18b87258bf96db3984358cb84458eedc35.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/pcdshub/lightpath/jobs/551039956.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test-environment/bin/doctr deploy . --built-docs docs/build/html --deploy-branch-name gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,10 +1,10 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.0+6.g2701e44',
+    VERSION: '0.5.1+2.gf99f9c1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false,
+    NAVIGATION_WITH_KEYS: false
 };
