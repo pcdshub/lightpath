2a31cee26ce17eb5de37814a4a1d1f6dca883a68
K Lauer <klauer@slac.stanford.edu>
2020-03-23 14:15:55 -0700

    STY: setup.py flake8

--- setup.py
+++ setup.py
@@ -1,21 +1,17 @@
 import versioneer
 from setuptools import (setup, find_packages)
 
-with open('requirements.txt') as f:
-        requirements = f.read().split()
+with open('requirements.txt', 'rt') as f:
+    requirements = f.read().splitlines()
 
-# Temporary hack until filestore is on PyPI, needed because
-# `pip install -r requirements.txt` works with git URLs, but `install_requires`
-# does not.
-requirements = [r for r in requirements if not r.startswith('git+')]
 
-setup(name     = 'lightpath',
-      version  = versioneer.get_version(),
-      cmdclass = versioneer.get_cmdclass(),
-      license  = 'BSD',
-      author   = 'SLAC National Accelerator Laboratory',
-
-      packages    = find_packages(),
-      include_package_data=True,
-      scripts=['bin/lightpath']
-    )
+setup(
+    name='lightpath',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    license='BSD',
+    author='SLAC National Accelerator Laboratory',
+    packages=find_packages(),
+    include_package_data=True,
+    scripts=['bin/lightpath']
+)
