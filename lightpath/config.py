0a70bfe112b07c766d6222bbd0a78326221172ba
Zachary Lentz <zlentz@slac.stanford.edu>
2020-09-10 09:42:33 -0700

    ENH: add TMO lightpath config

--- lightpath/config.py
+++ lightpath/config.py
@@ -16,4 +16,6 @@ beamlines = {'XPP': {'LFE': {},
              'CXI': {'LFE': {},
                      'HXD': {}},
              'MEC': {'LFE': {},
-                     'HXD': {'end': post_xrtm2h}}}
+                     'HXD': {'end': post_xrtm2h}},
+             'TMO': {'KFE': {}},
+            }
