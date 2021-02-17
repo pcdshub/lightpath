f722a6a2344245743c04e8cd570f817ff412be8e
Ken Lauer <klauer@slac.stanford.edu>
2021-02-17 10:12:43 -0800

    BLD: rework setup.py with console entrypoint, package_data

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
 import versioneer
-from setuptools import (setup, find_packages)
+from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-        requirements = f.read().split()
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
 
-# Temporary hack until filestore is on PyPI, needed because
-# `pip install -r requirements.txt` works with git URLs, but `install_requires`
-# does not.
-requirements = [r for r in requirements if not r.startswith('git+')]
-
-setup(name     = 'lightpath',
-      version  = versioneer.get_version(),
-      cmdclass = versioneer.get_cmdclass(),
-      license  = 'BSD',
-      author   = 'SLAC National Accelerator Laboratory',
-
-      packages    = find_packages(),
-      include_package_data=True,
-      scripts=['bin/lightpath']
-    )
+setup(
+    name="lightpath",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    license="BSD",
+    author="SLAC National Accelerator Laboratory",
+    packages=find_packages(),
+    package_data={
+        "lightpath": ["ui/*.ui"],
+    },
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'lightpath = lightpath.__main__:entrypoint',
+        ]
+    },
+)
