8e107fcfb3e5242ea725d3c70407980f0d3b1ad7
tangkong <tangkongrobert@gmail.com>
2022-07-11 10:26:30 -0700

    MNT: include LightpathState at top level

--- lightpath/__init__.py
+++ lightpath/__init__.py
@@ -2,6 +2,6 @@ __all__ = ['LightController', 'BeamPath']
 
 from . import _version
 from .controller import LightController
-from .path import BeamPath
+from .path import BeamPath, LightpathState  # noqa F401
 
 __version__ = _version.get_versions()['version']
