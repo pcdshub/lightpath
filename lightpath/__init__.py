61a04f6aa148385a9fdcfa89c232419cc2eb6c3b
tangkong <tangkongrobert@gmail.com>
2022-07-12 15:21:55 -0700

    MNT: Make LightpathState public

--- lightpath/__init__.py
+++ lightpath/__init__.py
@@ -1,7 +1,7 @@
-__all__ = ['LightController', 'BeamPath']
+__all__ = ['LightController', 'BeamPath', 'LightpathState']
 
 from . import _version
 from .controller import LightController
-from .path import BeamPath, LightpathState  # noqa F401
+from .path import BeamPath, LightpathState
 
 __version__ = _version.get_versions()['version']
