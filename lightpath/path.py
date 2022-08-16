779e24618cf9c4303cbc2c0423b54f77dae0e879
tangkong <tangkongrobert@gmail.com>
2022-08-16 09:53:56 -0700

    ENH: track transmission along path, update blocking device logic

--- lightpath/path.py
+++ lightpath/path.py
@@ -214,12 +214,14 @@ class BeamPath(OphydObject):
         Returns
         -------
         List[Device]
-            list of blockoing devices
+            list of blocking devices
         """
         # Cache important prior devices
         prev_device = None
         prev_status = None
         block = list()
+        current_transmission = 1
+
         for device in self.path:
             curr_state, curr_status = find_device_state(device)
 
@@ -234,13 +236,17 @@ class BeamPath(OphydObject):
                 block.append(prev_device)
             # check inserted
             elif curr_state is DeviceState.Inserted:
-                # Ignore devices with low enough transmssion
+                current_transmission *= curr_status.transmission
+                # Blocking if single device has low enough transmssion
                 if curr_status.transmission < self.minimum_transmission:
                     block.append(device)
-            # Find unknown and inconsistent devices
+                # Any device seeing current transmission below min would block
+                elif current_transmission < self.minimum_transmission:
+                    block.append(device)
+            # Do not add device to blocking list, it is removed
             elif curr_state is DeviceState.Removed:
                 pass
-            # Do not add device to blocking list, it is removed
+            # Unknown and inconsistent devices block
             else:
                 block.append(device)
 
