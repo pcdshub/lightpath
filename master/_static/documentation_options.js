7d22ea701d22dd56ad89b3983b3772e9921e6434
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2022-09-21 17:14:14 +0000

    Update docs after building Travis build 372 of pcdshub/lightpath
    The docs were built from the tag 'master' against the commit
    daec6071bf834a2341ea1c0565e34f4f980be1d6.
    
    The Travis build that generated this commit is at
    https://app.travis-ci.com/pcdshub/lightpath/jobs/583499902.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.8.13/bin/doctr deploy --build-tags master --deploy-branch-name gh-pages --command '"doctr-versions-menu"'

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.6.9+151.gea93b5e',
+    VERSION: '0.6.9+162.gdaec607',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
